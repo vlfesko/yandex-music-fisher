a62f63031d71978f14bcfc888df657c876d8c4f4
Tema <unlucky@inbox.ru>
2015-04-30 18:15:36 +0800

    обновлено API

--- src/js/yandex_api.js
+++ src/js/yandex_api.js
@@ -4,38 +4,16 @@
 var yandex = {};
 
 yandex.getTrackUrl = function (storageDir, success, fail) {
-    var url = '/api/v1.4/jsonp.xml?action=getTrackSrc&p=download-info/';
-    url += storageDir + '/2.mp3&r=' + Math.random();
-    utils.ajax(url, function (jsonp) {
-        var json;
-        jsonp = jsonp.replace('Ya.Music.Jsonp.', '');
-        var f = new Function('callback', jsonp);
-        try {
-            f(function (requestId, parsedJson) {
-                json = parsedJson[0];
-            });
-        } catch (e) {
-            fail('Не удалось распарсить jsonp. storageDir: ' + storageDir);
-            return;
-        }
-        var hosts = json['regional-hosts'];
-        hosts.push(json.host);
-        if (!hosts.length) {
-            fail('Не найдены хранилища трека. storageDir: ' + storageDir);
-            return;
-        }
-
+    var url = 'https://storage.mds.yandex.net/download-info/' + storageDir;
+    url += '/2?format=json&r=' + Math.random();
+    utils.ajax(url, function (json) {
         var md5 = utils.md5('XGRlBW9FXlekgbPrRHuSiA' + json.path.substr(1) + json.s);
-        var urlBody = '/get-mp3/' + md5 + '/' + json.ts + json.path;
-        var links = hosts.map(function (host) {
-            return 'http://' + host + urlBody;
-        });
-        success(links[0]);
+        success('https://' + json.host + '/get-mp3/' + md5 + '/' + json.ts + json.path);
     }, fail);
 };
 
 yandex.getTrack = function (trackId, success, fail) {
-    var url = '/handlers/track.jsx?track=' + trackId;
+    var url = 'https://music.yandex.ru/handlers/track.jsx?track=' + trackId;
     url += '&r=' + Math.random();
     utils.ajax(url, function (json) {
         success(json.track);
@@ -43,19 +21,19 @@ yandex.getTrack = function (trackId, success, fail) {
 };
 
 yandex.getArtist = function (artistId, success, fail) {
-    var url = '/handlers/artist.jsx?artist=' + artistId;
+    var url = 'https://music.yandex.ru/handlers/artist.jsx?artist=' + artistId;
     url += '&what=albums&r=' + Math.random();
     utils.ajax(url, success, fail);
 };
 
 yandex.getAlbum = function (albumId, success, fail) {
-    var url = '/handlers/album.jsx?album=' + albumId;
+    var url = 'https://music.yandex.ru/handlers/album.jsx?album=' + albumId;
     url += '&r=' + Math.random();
     utils.ajax(url, success, fail);
 };
 
 yandex.getPlaylist = function (username, playlistId, success, fail) {
-    var url = '/handlers/playlist.jsx?owner=' + username;
+    var url = 'https://music.yandex.ru/handlers/playlist.jsx?owner=' + username;
     url += '&kinds=' + playlistId;
     url += '&r=' + Math.random();
     utils.ajax(url, function (json) {
