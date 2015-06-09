fdf09633b2b9a79522611fbc58074c8da3ee06f5
Tema <unlucky@inbox.ru>
2015-06-09 19:25:48 +0800

    переключение вкладок, кнопка открытия загрузок

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,3 +1,23 @@
+'use strict';
+
+document.getElementById('addBtn').addEventListener('click', function () {
+    document.getElementById('downloadBtn').classList.remove('active');
+    this.classList.add('active');
+    document.getElementById('addContainer').classList.remove('hide');
+    document.getElementById('downloadContainer').classList.add('hide');
+});
+
+document.getElementById('downloadBtn').addEventListener('click', function () {
+    document.getElementById('addBtn').classList.remove('active');
+    this.classList.add('active');
+    document.getElementById('addContainer').classList.add('hide');
+    document.getElementById('downloadContainer').classList.remove('hide');
+});
+
+document.getElementById('downloadFolderBtn').addEventListener('click', function () {
+    chrome.downloads.showDefaultFolder();
+});
+
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('popup js');
+
 });
