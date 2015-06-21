a97fcd5a6e71338690648ceca812ca890eae8ca3
Tema <unlucky@inbox.ru>
2015-06-21 13:21:01 +0800

    kick this shit
    Оповещений не будет - даже в хроме они коряво работают, в
    яндекс.браузере вообще пропадают, поддерка кода усложняется.

--- src/js/downloader.js
+++ src/js/downloader.js
@@ -4,7 +4,6 @@
 var downloader = {
     queue: [],
     downloads: [],
-    notifications: [],
     activeThreadCount: 0
 };
 
@@ -35,7 +34,7 @@ downloader.getPrefix = function (i, max) {
 
 downloader.download = function () {
     var entity = downloader.queue.shift();
-    if (!entity) {
+    if (!entity) { // в очереди нет загрузок
         return;
     }
     switch (entity.type) {
@@ -88,44 +87,6 @@ downloader.download = function () {
                 });
 
             }, function (error) {
-                var nId = entity.options.notificationId;
-                downloader.notifications[nId].interruptedTracks.push(entity);
-                var notificationData = downloader.notifications[nId];
-                switch (entity.type) {
-                    case 'track':
-                        chrome.notifications.update(nId, {
-                            title: 'Загрузка прервана',
-                            buttons: [{
-                                    title: 'Отменить загрузку',
-                                    iconUrl: 'img/cancel.png'
-                                }, {
-                                    title: 'Повторить загрузку',
-                                    iconUrl: 'img/resume.png'
-                                }]
-                        }, function (wasUpdated) {
-                        });
-                        break;
-                    case 'album_track':
-                    case 'playlist_track':
-                        chrome.notifications.update(nId, {
-                            buttons: [{
-                                    title: 'Отменить загрузку',
-                                    iconUrl: 'img/cancel.png'
-                                }, {
-                                    title: 'Повторить загрузку',
-                                    iconUrl: 'img/resume.png'
-                                }]
-                        }, function (wasUpdated) {
-                        });
-                        var interruptedCount = notificationData.interruptedTracks.length;
-                        if (notificationData.trackCount + interruptedCount === notificationData.totalTrackCount) {
-                            chrome.notifications.update(nId, {
-                                title: 'Загрузка частично прервана (загружено ' + notificationData.trackCount + ' из ' + notificationData.totalTrackCount + ')'
-                            }, function (wasUpdated) {
-                            });
-                        }
-                        break;
-                }
                 logger.addMessage(error);
                 downloader.activeThreadCount--;
                 downloader.download();
@@ -141,8 +102,6 @@ downloader.download = function () {
                 downloader.downloads[downloadId] = entity;
             });
             break;
-        default:
-            logger.addMessage('Неизвестный тип загрузки: ' + entity.type);
     }
 };
 
@@ -159,33 +118,8 @@ downloader.add = function (type, cargo, options) {
 };
 
 downloader.downloadTrack = function (track) {
-    var notificationId = 'track#' + track.id;
-    var artists = utils.parseArtists(track.artists);
-    var iconUrl = 'img/icon.png';
 
-    downloader.add('track', track, {
-        notificationId: notificationId
-    });
-
-    if (track.albums[0].coverUri) {
-        iconUrl = 'https://' + track.albums[0].coverUri.replace('%%', '100x100');
-    }
-    chrome.notifications.create(notificationId, {
-        type: 'progress',
-        iconUrl: iconUrl,
-        title: 'Загрузка...',
-        message: artists + ' - ' + track.title,
-        contextMessage: 'Трек (' + utils.bytesToStr(track.fileSize) + ' - ' + utils.durationToStr(track.durationMs) + ')',
-        progress: 0,
-        buttons: [{
-                title: 'Отменить загрузку',
-                iconUrl: 'img/cancel.png'
-            }]
-    }, function (notificationId) {
-        downloader.notifications[notificationId] = {
-            interruptedTracks: []
-        };
-    });
+    downloader.add('track', track);
 };
 
 downloader.downloadAlbum = function (album, discographyArtist) {
@@ -193,7 +127,6 @@ downloader.downloadAlbum = function (album, discographyArtist) {
         logger.addMessage('Пустой альбом. album.id:' + album.id);
         return;
     }
-    var notificationId = 'album#' + album.id;
     var artists = utils.parseArtists(album.artists);
     if (album.version) {
         album.title += ' (' + album.version + ')';
@@ -205,15 +138,11 @@ downloader.downloadAlbum = function (album, discographyArtist) {
     var totalSize = 0;
     var totalDuration = 0;
     var totalTrackCount = album.trackCount;
-    var iconUrl = 'img/icon.png';
 
     if (storage.current.shouldDownloadCover && album.coverUri) {
-        iconUrl = 'https://' + album.coverUri.replace('%%', '100x100');
         downloader.add('cover', {
             url: 'https://' + album.coverUri.replace('%%', storage.current.albumCoverSize),
             filename: saveDir + '/cover.jpg'
-        }, {
-            notificationId: notificationId
         });
     }
 
@@ -223,7 +152,7 @@ downloader.downloadAlbum = function (album, discographyArtist) {
         for (i = 0; i < album.volumes.length; i++) {
             for (var j = 0; j < album.volumes[i].length; j++) {
                 track = album.volumes[i][j];
-                if (track.error) {
+                if (track.error) { // todo: проверить, если ли сейчас такое поле
                     totalTrackCount--;
                     logger.addMessage('Ошибка: ' + track.error + '. trackId: ' + track.id);
                     continue;
@@ -232,8 +161,7 @@ downloader.downloadAlbum = function (album, discographyArtist) {
                 totalDuration += track.durationMs;
                 downloader.add('album_track', track, {
                     saveDir: saveDir + '/CD' + (i + 1),
-                    namePrefix: downloader.getPrefix(j + 1, album.volumes[i].length),
-                    notificationId: notificationId
+                    namePrefix: downloader.getPrefix(j + 1, album.volumes[i].length)
                 });
             }
         }
@@ -249,31 +177,10 @@ downloader.downloadAlbum = function (album, discographyArtist) {
             totalDuration += track.durationMs;
             downloader.add('album_track', track, {
                 saveDir: saveDir,
-                namePrefix: downloader.getPrefix(i + 1, album.volumes[0].length),
-                notificationId: notificationId
+                namePrefix: downloader.getPrefix(i + 1, album.volumes[0].length)
             });
         }
     }
-
-    chrome.notifications.create(notificationId, {
-        type: 'progress',
-        iconUrl: iconUrl,
-        title: 'Загрузка (0 из ' + totalTrackCount + ')...',
-        message: artists + ' - ' + album.title,
-        contextMessage: 'Альбом (' + utils.bytesToStr(totalSize) + ' - ' + utils.durationToStr(totalDuration) + ')',
-        progress: 0,
-        buttons: [{
-                title: 'Отменить загрузку',
-                iconUrl: 'img/cancel.png'
-            }]
-    }, function (notificationId) {
-        downloader.notifications[notificationId] = {
-            trackCount: 0,
-            totalTrackCount: totalTrackCount,
-            interruptedTracks: [],
-            saveDir: saveDir
-        };
-    });
 };
 
 downloader.downloadPlaylist = function (playlist) {
@@ -283,7 +190,6 @@ downloader.downloadPlaylist = function (playlist) {
         logger.addMessage(message);
         return;
     }
-    var notificationId = 'playlist#' + playlist.owner.login + '#' + playlist.kind;
     var saveDir = downloader.clearPath(playlist.title);
     var totalSize = 0;
     var totalDuration = 0;
@@ -300,44 +206,9 @@ downloader.downloadPlaylist = function (playlist) {
         totalDuration += track.durationMs;
         downloader.add('playlist_track', track, {
             saveDir: saveDir,
-            namePrefix: downloader.getPrefix(i + 1, playlist.tracks.length),
-            notificationId: notificationId
+            namePrefix: downloader.getPrefix(i + 1, playlist.tracks.length)
         });
     }
-
-    var iconUrl = 'img/icon.png';
-    if (playlist.cover) {
-        switch (playlist.cover.type) {
-            case 'pic':
-                iconUrl = 'https://' + playlist.cover.uri.replace('%%', '100x100');
-                break;
-            case 'mosaic':
-                iconUrl = 'https://' + playlist.cover.itemsUri[0].replace('%%', '100x100');
-                break;
-            default:
-                logger.addMessage('Неизвестный тип обложки: ' + playlist.cover.type);
-        }
-    }
-
-    chrome.notifications.create(notificationId, {
-        type: 'progress',
-        iconUrl: iconUrl,
-        title: 'Загрузка (0 из ' + totalTrackCount + ')...',
-        message: playlist.title,
-        contextMessage: 'Плейлист (' + utils.bytesToStr(totalSize) + ' - ' + utils.durationToStr(totalDuration) + ')',
-        progress: 0,
-        buttons: [{
-                title: 'Отменить загрузку',
-                iconUrl: 'img/cancel.png'
-            }]
-    }, function (notificationId) {
-        downloader.notifications[notificationId] = {
-            trackCount: 0,
-            totalTrackCount: totalTrackCount,
-            interruptedTracks: [],
-            saveDir: saveDir
-        };
-    });
 };
 
 downloader.onChange = function (delta) {
@@ -348,86 +219,6 @@ downloader.onChange = function (delta) {
     }
     if (!delta.state) {
         return;
-    }
-    if (entity.type === 'opener') {
-        if (delta.state.current === 'complete') {
-            chrome.downloads.show(delta.id);
-            chrome.downloads.removeFile(delta.id);
-            chrome.downloads.erase({
-                id: delta.id
-            });
-            delete(downloader.downloads[delta.id]);
-        }
-        return;
-    }
-    var nId = entity.options.notificationId;
-    var notificationData = downloader.notifications[nId];
-    switch (entity.type) {
-        case 'track':
-            if (delta.state.current === 'complete') {
-                chrome.notifications.update(nId, {
-                    title: 'Загрузка завершена',
-                    progress: 100,
-                    buttons: []
-                }, function (wasUpdated) {
-                });
-            } else if (delta.state.current === 'interrupted') {
-                downloader.notifications[nId].interruptedTracks.push(downloader.downloads[delta.id]);
-                chrome.notifications.update(nId, {
-                    title: 'Загрузка прервана',
-                    buttons: [{
-                            title: 'Отменить загрузку',
-                            iconUrl: 'img/cancel.png'
-                        }, {
-                            title: 'Повторить загрузку',
-                            iconUrl: 'img/resume.png'
-                        }]
-                }, function (wasUpdated) {
-                });
-            }
-            break;
-        case 'album_track':
-        case 'playlist_track':
-            if (delta.state.current === 'complete') {
-                downloader.notifications[nId].trackCount++;
-                if (notificationData.trackCount === notificationData.totalTrackCount) {
-                    chrome.notifications.update(nId, {
-                        title: 'Загрузка завершена',
-                        progress: 100,
-                        buttons: []
-                    }, function (wasUpdated) {
-                    });
-                } else {
-                    var progress = Math.round(notificationData.trackCount / notificationData.totalTrackCount * 100);
-                    chrome.notifications.update(nId, {
-                        title: 'Загрузка (' + notificationData.trackCount + ' из ' + notificationData.totalTrackCount + ')...',
-                        progress: progress
-                    }, function (wasUpdated) {
-                    });
-                }
-            } else if (delta.state.current === 'interrupted') {
-                downloader.notifications[nId].interruptedTracks.push(downloader.downloads[delta.id]);
-                chrome.notifications.update(nId, {
-                    buttons: [{
-                            title: 'Отменить загрузку',
-                            iconUrl: 'img/cancel.png'
-                        }, {
-                            title: 'Повторить загрузку',
-                            iconUrl: 'img/resume.png'
-                        }]
-                }, function (wasUpdated) {
-                });
-            }
-            var interruptedCount = notificationData.interruptedTracks.length;
-            if (interruptedCount && notificationData.trackCount + interruptedCount === notificationData.totalTrackCount) {
-                chrome.notifications.update(nId, {
-                    title: 'Загрузка прервана (загружено ' + notificationData.trackCount + ' из ' + notificationData.totalTrackCount + ')'
-                }, function (wasUpdated) {
-                });
-            }
-            break;
-        case 'cover':
-            break;
     }
     downloader.activeThreadCount--;
     delete(downloader.downloads[delta.id]);
