fa83754f405235857c0c8e9c54cbe88cf3f2a170
Tema <kukukov.kuku@gmail.com>
2015-10-09 11:25:15 +0800

    update show_download_stats.js

--- utils/show_download_stats.js
+++ utils/show_download_stats.js
@@ -2,13 +2,32 @@
 
 'use strict';
 
-utils.ajax('https://api.github.com/repos/egoroof/yandex-music-fisher/releases', 'json', function (releases) {
+var githubURL = 'https://api.github.com/repos/egoroof/yandex-music-fisher/releases';
+var bitbucketURL = 'https://bitbucket.org/api/2.0/repositories/egoroof/yandex-music-fisher/downloads';
+
+function onError(error, details) {
+    console.log(error, details);
+}
+
+function onGithubLoaded(releases) {
+    console.log('GitHub statistics');
     var totalDownloadCount = 0;
     releases.forEach(function (release) {
         console.log(release.name, release.assets[0].download_count);
         totalDownloadCount += release.assets[0].download_count;
     });
     console.log('total', totalDownloadCount);
-}, function (error, details) {
-    console.log(error, details);
-});
+}
+
+function onBitbucketLoaded(info) {
+    console.log('Bitbucket statistics');
+    var totalDownloadCount = 0;
+    info.values.forEach(function (download) {
+        console.log(download.name, download.downloads);
+        totalDownloadCount += download.downloads;
+    });
+    console.log('total', totalDownloadCount);
+}
+
+utils.ajax(githubURL, 'json', onGithubLoaded, onError);
+utils.ajax(bitbucketURL, 'json', onBitbucketLoaded, onError);
